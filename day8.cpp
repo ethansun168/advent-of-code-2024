0fc62bd43cdcd9f4d701c3debca64b29ae6f082d
ethansun <ethansun@umich.edu>
2024-12-30 00:31:19 -0500

    merged day8

--- day8.cpp
+++ day8.cpp
@@ -4,6 +4,7 @@
 #include <unordered_map>
 #include <sstream>
 #include <set>
+#include <functional>
 
 bool inMap(const std::pair<int, int>& coord, int rowSize, int colSize) {
     // filter out antinodes that are off the map < 0 or > rowSize, > colSize
@@ -18,7 +19,14 @@ bool inMap(const std::pair<int, int>& coord, int rowSize, int colSize) {
     return true;
 }
 
-void antinode(std::set<std::pair<int, int>>& antinodes, std::pair<int, int> one, std::pair<int, int> two, int rowSize, int colSize) {
+void part1
+(
+    std::set<std::pair<int, int>>& antinodes,
+    std::pair<int, int> one, std::pair<int, int> two,
+    int rowSize,
+    int colSize
+) {
+    // each pair of antennas produce two antinodes
     // (3,4) (5,5)
     // 2 - 1 --> (2, 1)
     // one - diff --> (1, 3)
@@ -27,26 +35,48 @@ void antinode(std::set<std::pair<int, int>>& antinodes, std::pair<int, int> one,
     std::pair<int, int> node1 = {one.first - diff.first, one.second - diff.second};
     std::pair<int, int> node2 = {two.first + diff.first, two.second + diff.second};
     
+    if (inMap(node1, rowSize, colSize)) {
+        antinodes.insert(node1);
+    }
+
+    if (inMap(node2, rowSize, colSize)) {
+        antinodes.insert(node2);
+    }
+}
+
+void part2
+(
+    std::set<std::pair<int, int>>& antinodes,
+    std::pair<int, int> one,
+    std::pair<int, int> two,
+    int rowSize,
+    int colSize
+) {
+    // (3,4) (5,5)
+    // 2 - 1 --> (2, 1)
+    // one - diff --> (1, 3)
+    // two + diff --> (7, 6)
+    std::pair<int, int> diff = {two.first - one.first, two.second - one.second};
+    std::pair<int, int> node1 = {one.first - diff.first, one.second - diff.second};
+    std::pair<int, int> node2 = {two.first + diff.first, two.second + diff.second};
     while (inMap(node1, rowSize, colSize)) {
         antinodes.insert(node1);
         node1 = {node1.first - diff.first, node1.second - diff.second};
     }
-
     while (inMap(node2, rowSize, colSize)) {
         antinodes.insert(node2);
         node2 = {node2.first + diff.first, node2.second + diff.second};
     }
-
-    // if (inMap(node1, rowSize, colSize)) {
-    //     antinodes.insert(node1);
-    // }
-
-    // if (inMap(node2, rowSize, colSize)) {
-    //     antinodes.insert(node2);
-    // }
 }
 
-std::set<std::pair<int, int>> findAntinodes(const std::unordered_map<char, std::vector<std::pair<int, int>>>& antennas, int rowSize, int colSize) {
+std::set<std::pair<int, int>> findAntinodes
+(
+    const std::unordered_map<char,
+    std::vector<std::pair<int, int>>>& antennas,
+    int rowSize,
+    int colSize,
+    std::function<void(std::set<std::pair<int, int>>&, std::pair<int, int>, std::pair<int, int>, int rowSize, int colSize)> antinode
+) {
     std::set<std::pair<int, int>> antinodes;
     for (const auto& [key, coords] : antennas) {
         for(int i = 0; i < coords.size() - 1; ++i) {
@@ -79,8 +109,11 @@ int main(int argc, char** argv) {
         }
         ++row;
     }
+    
+    std::set<std::pair<int, int>> antinodes = findAntinodes(antennas, row - 1, col - 1, part1);
+    std::cout << "Part 1: " << antinodes.size() << std::endl;
 
-    std::set<std::pair<int, int>> antinodes = findAntinodes(antennas, row - 1, col - 1);
+    antinodes = findAntinodes(antennas, row - 1, col - 1, part2);
 
     // insert antennas
     for (const auto& [key, value] : antennas) {
@@ -91,5 +124,5 @@ int main(int argc, char** argv) {
         }
     }
 
-    std::cout << antinodes.size() << std::endl;
+    std::cout << "Part 2: " << antinodes.size() << std::endl;
 }
